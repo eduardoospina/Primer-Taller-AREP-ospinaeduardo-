35311c1884cf23bbe39087dd0441861729262c8f
unknown <eduardo.ospina@labinfo.is.escuelaing.edu.co>
2022-02-08 14:12:04 -0500

    butun calcular funcionando

--- src/main/resources/paginainicio/js/app.js
+++ src/main/resources/paginainicio/js/app.js
@@ -11,3 +11,17 @@ celsiusafahrenheit.addEventListener('click', function(){
             console.log(obj);
             })
 });
+
+var celsiusafahrenheit = document.getElementById('button-celsiusafahrenheit');
+celsiusafahrenheit.addEventListener('click', function(){
+    var celda = document.getElementById('Celsius').value;
+    console.log(celda);
+    var linkapi1 = 'https://calc-api-be.herokuapp.com/Calculator'
+    fetch(`${linkapi1}/celsius/${celda}`).then(res => {
+        return res.json();
+        }).then( rta =>  {
+            var obj = JSON.parse(rta.fahrenheit);
+            $("#fahrenheit").text("Fahrenheit : " + obj);
+            console.log(obj);
+            })
+});
