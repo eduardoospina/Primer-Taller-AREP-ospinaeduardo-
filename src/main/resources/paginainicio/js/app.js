5bd32a4e6c318ad360ef6490fcd4a0134ac0207b
Eduardo Ospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-02-08 19:17:08 -0500

    terminar operacion web

--- src/main/resources/paginainicio/js/app.js
+++ src/main/resources/paginainicio/js/app.js
@@ -12,16 +12,16 @@ celsiusafahrenheit.addEventListener('click', function(){
             })
 });
 
-var celsiusafahrenheit = document.getElementById('button-celsiusafahrenheit');
+var fahrenheitacelsius = document.getElementById('button-fahrenheitacelsius');
 celsiusafahrenheit.addEventListener('click', function(){
-    var celda = document.getElementById('Celsius').value;
+    var celda = document.getElementById('fahrenheits').value;
     console.log(celda);
     var linkapi1 = 'https://calc-api-be.herokuapp.com/Calculator'
     fetch(`${linkapi1}/celsius/${celda}`).then(res => {
         return res.json();
         }).then( rta =>  {
             var obj = JSON.parse(rta.fahrenheit);
-            $("#fahrenheit").text("Fahrenheit : " + obj);
+            $("#celsiusl").text("Celsius : " + obj);
             console.log(obj);
             })
 });
